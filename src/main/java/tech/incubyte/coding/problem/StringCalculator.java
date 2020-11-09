429183a6a2006b7a5f3a0e8004cd2a7b0503b9b2
SaiTalamarla <saikishantalamarla@gmail.com>
2020-11-09 17:28:25 +0530

    TestCase1-Returns Number for String of Length greater than 1-- Code Refactored

--- src/main/java/tech/incubyte/coding/problem/StringCalculator.java
+++ src/main/java/tech/incubyte/coding/problem/StringCalculator.java
@@ -6,12 +6,28 @@ public class StringCalculator
     
 
 	public int add(String numbersGiven) {
-		
-		if (numbersGiven.isEmpty())
+        
+        if (numbersGiven.isEmpty())
             return 0;
-		else if(numbersGiven.length()==1){
-            return Integer.parseInt(numbersGiven);
+        else 
+            return sumOfNumbers(numbersGiven);
+
+    }
+
+    public int sumOfNumbers(String numbersGiven) {
+        String[] inputNumbers = new String[numbersGiven.length()];
+        int numbersSum = 0;
+        
+        inputNumbers = splitLogic(numbersGiven);
+        for (int i = 0; i < inputNumbers.length; i++) {
+            if (inputNumbers[i] != null && !inputNumbers[i].isEmpty())
+                    numbersSum += Integer.parseInt(inputNumbers[i]);
         }
-		return -1;
-	}
+        
+        return numbersSum;
+    }
+
+    public String[] splitLogic(String numbersGiven) {
+        return numbersGiven.split(",");
+    }
 }
